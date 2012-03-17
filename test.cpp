5f51c4df51c568010fccdc861f9b17de259de5c7
Allan Pendergrast <robalan@gmail.com>
2012-03-16 20:44:02 -0400

    Plasma! Moving windows! Changing resolutions!

--- test.cpp
+++ test.cpp
@@ -1,23 +1,43 @@
 #include <GL/glew.h>
 #include <GL/glut.h>
 #include <stdio.h>
+#include <time.h>
+#include <math.h>
 
-const char *vshad_str[] = {"#version 140\n\
-uniform mat4 MVPMatrix; in vec4 mcVertex;\n\
-void main() { gl_Position = MVPMatrix * mcVertex; }"};
+const char *vshad_str[] = {"#version 120\n\
+void main() { gl_Position = ftransform(); }"};
 const char *fshad_str[] = {"#version 140\n\
-out vec3 FragmentColor;\n\
-void main() { FragmentColor = vec3(1.0, 1.0, 1.0); }"};
+uniform int width;\
+uniform int height;\
+uniform int time;\
+uniform int char_w;\
+uniform int char_h;\
+void main() { float xchar = floor(gl_FragCoord.x/char_w)*char_w;\
+              float ychar = floor(gl_FragCoord.y/char_h)*char_h;\
+              float color = (sin(sqrt(pow(width/2-xchar,2)+pow(height/2-ychar,2))/50-time/300.0)+\
+                                  sin((xchar+ychar)/30+time/230.0)+\
+								  sin(xchar/37-time/600.0) + 1.5)/4.5;\
+              if(color < 0.167) gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\
+			  else if(color < 0.333) gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);\
+			  else if(color < 0.5) gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);\
+			  else if(color < 0.667) gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);\
+			  else if(color < 0.833) gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);\
+			  else gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0); }"};
 
 void reshape(int w, int h);
 void render(void);
+void animate(void);
+
+GLuint program;
+int char_w;
+int char_h;
 
 int main(int argc, char *argv[])
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(320, 320);
+	glutInitWindowSize(1000, 700);
 	glutCreateWindow("test");
 	GLenum err = glewInit();
 	
@@ -63,7 +83,7 @@ int main(int argc, char *argv[])
 	glGetShaderInfoLog(fshad, 1024, NULL, log);
 	printf("fragment shader compile log: %s\n", log);
 	
-	GLuint program = glCreateProgram();
+	program = glCreateProgram();
 	glAttachShader(program, vshad);
 	glAttachShader(program, fshad);
 	glLinkProgram(program);
@@ -71,12 +91,16 @@ int main(int argc, char *argv[])
 	printf("program link log: %s\n", log);
 	
 	printf("empty logs are good! means success.\n");
-	
+
 	glUseProgram(program);
 	glDeleteShader(vshad);
 	glDeleteShader(fshad);
 	
+	char_w = 8;
+	char_h = 14;
+
 	glutDisplayFunc(render);
+	glutIdleFunc(animate);
 	glutReshapeFunc(reshape);
 	glutMainLoop();
 	
@@ -94,19 +118,54 @@ void reshape(int w, int h)
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glViewport(0, 0, w, h);
-	gluPerspective(80, ratio, 1, 100);
-	glMatrixMode(GL_MODELVIEW);
+	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 }
 void render(void)
 {
 	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 	
-	glBegin(GL_TRIANGLES);
-		glVertex3f(-2, -2, -5);
-		glVertex3f(2, 0, -5);
-		glVertex3f(0, 2, -5);
+	float windowW = glutGet(GLUT_WINDOW_WIDTH);
+	float windowH = glutGet(GLUT_WINDOW_HEIGHT);
+
+	float consoleX = 0.5 + 0.2*sin(clock()/2000.0);
+	float consoleY = 0.5 + 0.2*cos(clock()/2000.0);
+	
+	// Clamp console to character borders
+	consoleX = (floor(consoleX * windowW / char_w) * char_w) / windowW;
+	consoleY = (floor(consoleY * windowH / char_h) * char_h + char_h/2) / windowH;
+	// Should be 640x350
+	float consoleW = 640.0/windowW;
+	float consoleH = 350.0/windowH;
+
+	glBegin(GL_QUADS);
+		glVertex3f(consoleX-consoleW/2, consoleY-consoleH/2, -1);
+		glVertex3f(consoleX+consoleW/2, consoleY-consoleH/2, -1);
+		glVertex3f(consoleX+consoleW/2, consoleY+consoleH/2, -1);
+		glVertex3f(consoleX-consoleW/2, consoleY+consoleH/2, -1);
 	glEnd();
 	
 	glutSwapBuffers();
 }
 
+void animate(void)
+{	
+	int t = clock();
+
+	if (t > 5000)  { char_w = 4; char_h = 7; }
+	if (t > 10000) { char_w = 3; char_h = 4; }
+	if (t > 13000) { char_w = 2; char_h = 2; }
+	if (t > 15000) { char_w = 1; char_h = 1; }
+
+	GLint w_var = glGetUniformLocation(program, "width");
+	glUniform1i(w_var, glutGet(GLUT_WINDOW_WIDTH));
+	GLint h_var = glGetUniformLocation(program, "height");
+	glUniform1i(h_var, glutGet(GLUT_WINDOW_HEIGHT));
+	GLint t_var = glGetUniformLocation(program, "time");
+	glUniform1i(t_var, t);
+	GLint char_w_var = glGetUniformLocation(program, "char_w");
+	glUniform1i(char_w_var, char_w);
+	GLint char_h_var = glGetUniformLocation(program, "char_h");
+	glUniform1i(char_h_var, char_h);
+
+	render();
+}
