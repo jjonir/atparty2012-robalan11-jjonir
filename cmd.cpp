f7807d83843916d2e1634e4b3301a48da53c3d54
James Pendergrast <jjonir@gmail.com>
2012-03-19 16:11:26 -0400

    Finished scroll bar. Still some to do on title bar.

--- cmd.cpp
+++ cmd.cpp
@@ -2,6 +2,9 @@
 #include <GL/glut.h>
 #include <math.h>
 
+// TODO everything's shifted up by 1 pixel (MinGW, I don't think it was on native gcc)
+// fix this
+
 GLuint cmd_program;
 
 void cmd_render(void)
@@ -62,6 +65,22 @@ const char *cmd_fshad =
 "		COLORI(172, 168, 153);"
 "	else if(rel_pos.x == 1 || rel_pos.x == 35 || rel_pos.x == 17 || rel_pos.y == 19)" //buttons tl 0
 "		COLORI(241, 239, 226);"
+"	else if(rel_pos.x >= 4 && rel_pos.x <= 9 && rel_pos.y >= 7 && rel_pos.y <= 8)"
+"		COLORI(0, 0, 0);"
+"	else if((rel_pos.x == 18 || rel_pos.x == 27) && rel_pos.y >= 7 && rel_pos.y <= 15)"
+"		COLORI(0, 0, 0);"
+"	else if(rel_pos.x >= 19 && rel_pos.x <= 26 && (rel_pos.y == 7 || rel_pos.y == 14 || rel_pos.y == 15))"
+"		COLORI(0, 0, 0);"
+"	else if((rel_pos.x == 38 || rel_pos.x == 45) &&"
+"			(rel_pos.y == 7 || rel_pos.y == 8 || rel_pos.y == 13 || rel_pos.y == 14))"
+"		COLORI(0, 0, 0);"
+"	else if((rel_pos.x == 39 || rel_pos.x == 44) &&"
+"			((rel_pos.y >=7 && rel_pos.y <=9) || (rel_pos.y >= 12 && rel_pos.y <= 14)))"
+"		COLORI(0, 0, 0);"
+"	else if((rel_pos.x == 40 || rel_pos.x == 43) && rel_pos.y >= 8 && rel_pos.y <= 13)"
+"		COLORI(0, 0, 0);"
+"	else if((rel_pos.x == 41 || rel_pos.x == 42) && rel_pos.y >= 9 && rel_pos.y <= 12)"
+"		COLORI(0, 0, 0);"
 "	else if(!(rel_pos.x == 32 || rel_pos.x == 33))"
 "		COLORI(236, 233, 216);"
 "	else" //TODO button labels
@@ -69,7 +88,29 @@ const char *cmd_fshad =
 "	return rv;"
 "}"
 "void colorScrollBar(ivec2 rel_pos) {"
-"	COLORI(0, 255, 0);"
+"	if(rel_pos.y >= 17 && rel_pos.y <= cmd_size.y-38-40)"
+"		if(fract(float(rel_pos.x+rel_pos.y)/2) == 0.5)"
+"			COLORI(236, 233, 216);"
+"		else"
+"			COLORI(255, 255, 255);"
+"	else if(rel_pos.x == 16 || rel_pos.y == 0 || rel_pos.y == cmd_size.y-38-17 || rel_pos.y == cmd_size.y-38-39)"
+"		COLORI(113, 111, 100);"
+"	else if(rel_pos.x == 0 || rel_pos.y == 16 || rel_pos.y == cmd_size.y-38-1 || rel_pos.y == cmd_size.y-38-18)"
+"		COLORI(241, 239, 226);"
+"	else if(rel_pos.x == 15 || rel_pos.y == 1 || rel_pos.y == cmd_size.y-38-16 || rel_pos.y == cmd_size.y-38-38)"
+"		COLORI(172, 168, 153);"
+"	else if(rel_pos.x == 1 || rel_pos.y == 15 || rel_pos.y == cmd_size.y-38-2 || rel_pos.y == cmd_size.y-38-19)"
+"		COLORI(255, 255, 255);"
+"	else if((rel_pos.y == 9 || rel_pos.y == cmd_size.y-38-10) && rel_pos.x >= 5 && rel_pos.x <= 11)"
+"		COLORI(0, 0, 0);"
+"	else if((rel_pos.y == 8 || rel_pos.y == cmd_size.y-38-9) && rel_pos.x >= 6 && rel_pos.x <= 10)"
+"		COLORI(0, 0, 0);"
+"	else if((rel_pos.y == 7 || rel_pos.y == cmd_size.y-38-8) && rel_pos.x >= 7 && rel_pos.x <= 9)"
+"		COLORI(0, 0, 0);"
+"	else if((rel_pos.y == 6 || rel_pos.y == cmd_size.y-38-7) && rel_pos.x == 8)"
+"		COLORI(0, 0, 0);"
+"	else"
+"		COLORI(236, 233, 216);"
 "}"
 "void main() {"
 "	ivec2 rel_pos = ivec2(gl_FragCoord.xy) - cmd_pos;"
@@ -93,8 +134,17 @@ const char *cmd_fshad =
 "			((rel_pos.x >= cmd_size.x-56 && rel_pos.x <= cmd_size.x-25) ||"
 "			(rel_pos.x >= cmd_size.x-22 && rel_pos.x <= cmd_size.x-7)) &&"
 "			colorButtons(rel_pos-cmd_size+ivec2(56, 27)));"
-"	else if(rel_pos.x >= 4 && rel_pos.x <= cmd_size.x-5 && rel_pos.y >= cmd_size.y-29 && rel_pos.y <= cmd_size.y-5)"
-"		COLORI(0, 0, 255);"
+"	else if(rel_pos.y >= cmd_size.y-29 && rel_pos.y <= cmd_size.y-5) {"
+"		if(rel_pos.x >= 4 && rel_pos.x <= 37)"
+"			COLORI(0, 84, 227);"
+"		else if(rel_pos.x >= cmd_size.x-58 && rel_pos.x <= cmd_size.x-5)"
+"			COLORI(61, 149, 255);"
+"		else {"
+"			float p = float(rel_pos.x-37)/float(cmd_size.x-58-37);"
+"			gl_FragColor = mix(vec4(0, 84/256.0, 227/256.0, 1),"
+"				vec4(61/256.0, 149/256.0, 255/256.0, 1), vec4(p,p,p,p));"
+"		}"
+"	}"
 /* inner border */
 "	else if(rel_pos.x == cmd_size.x-5 || rel_pos.y == 4)" //br 4
 "		COLORI(255, 255, 255);"
@@ -106,9 +156,11 @@ const char *cmd_fshad =
 "		COLORI(113, 111, 110);"
 /* scroll bar */
 "	else if(rel_pos.x >= cmd_size.x-23)"
-"		colorScrollBar(rel_pos-cmd_size+ivec2(23, 7));"
+"		colorScrollBar(rel_pos-ivec2(cmd_size.x-23, 6));"
+/* text area */
 "	else if(rel_pos.x >= 0 && rel_pos.x <= cmd_size.x-1 && rel_pos.y >= 0 && rel_pos.y <= cmd_size.y-1)"
 "		COLORI(0, 0, 0);"
+/* everything else, there should not be anything else, account for it somehow! */
 "	else"
 "		gl_FragColor = vec4(1, 0, 0, 1);"
 "}"
